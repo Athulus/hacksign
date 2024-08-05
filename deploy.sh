b306faa3418d2961468683d8ba334f6aaf446719
Max Redmond <athulus1991@gmail.com>
2024-08-05 15:18:34 -0400

    workign systemd service stuff
    this deploy script is specific to the existing virtual environment on the server

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,3 @@
-pipenv run deploy
+#!/usr/bin/env sh
+
+/home/max/.local/share/virtualenvs/hacksign-Q6Ya1UPn/bin/python /home/max/.local/share/virtualenvs/hacksign-Q6Ya1UPn/bin/gunicorn -b 0.0.0.0 hacksign:app
